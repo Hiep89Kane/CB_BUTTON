50dc0efd5cf64a36185fed6797a9bd3bfa4ddc2f
HIEP_PC <gacononline07bk0@gmail.com>
2022-08-25 10:21:26 +0700

    them Irq timer

--- application.c
+++ application.c
@@ -84,3 +84,20 @@ void appButtonsCallBack_init()
 	//init callback of event button0
 	BUTTON_arr[0].cb_Event = &userButton0_event;
 }
+
+
+/**
+ * @brief  SYSTICK callback - 1ms.
+ * @retval None
+ */
+void HAL_SYSTICK_Callback(void)
+{
+	static uint8_t tx10;
+
+	// 10ms
+	if (++tx10 >= 10)
+	{
+		tx10 = 0;
+		timer_periodic_poll();
+	}
+}
